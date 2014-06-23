d4173ffd99b01b08c2dce58c91058116b7e1bcf1
Teri <tealynsea@hotmail.com>
2014-06-23 13:42:26 -0600

    first completed commit

---
+++ main.js
@@ -0,0 +1,59 @@
+
+/* Example from practice
+
+
+for(var i=0; i<5; i++) {
+	console.log(i);
+}*/
+
+/*problem 1
+for(var i=0; i<600; i+=100) {
+	console.log(i);
+}
+*/
+
+
+/*problem 2
+for(var i=1; i<65; i*=2) {
+	console.log(i);
+}
+*/
+
+
+/*problem 3
+for(var i=1; i<4; i++) {
+	for(var j=0; j<3; j++){
+
+		console.log(i);
+	}
+	
+}*/
+
+/* problem 4
+for(var i=0; i<11; i+=2) {
+	console.log(i);
+}*/
+
+
+/*problem 5
+for(var i=3; i<16; i+=3) {
+
+	console.log(i);
+}*/
+
+
+/*
+for(var i=9; i>-1; i--) {
+	console.log(i)
+}*/
+
+
+/*
+for(var i=1; i<4; i++) {
+	for(var j=0; j<3; j++) {
+		console.log(i)
+	}
+}
+*/
+
+
